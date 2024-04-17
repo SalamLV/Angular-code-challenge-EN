aa5735d3df8840af3ddaa6aee72caf0c2880b501
Aleksejs Demjanenko <aleksejs.demjanenko@gmail.com>
2024-04-17 21:02:06 +0300

    Initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+  vehicleSubtypes: { [key: string]: string[] } = {
+    Auto: [
+      'Hatchback',
+      'Sedan',
+      'Station',
+      'Cabriolet',
+      'Coupé',
+      'Multi Purpose Vehicle (MVP)',
+      'Terreinauto',
+    ],
+    Motor: [
+      'All-road',
+      'Naked',
+      'Enduro',
+      'Race',
+      'Toermotor',
+      'Chopper',
+      'Zijspan',
+    ],
+    Scooter: [],
+  };
+  currentSubtypes: string[] = [];
+  imagePath: string = '/assets/auto.jpg';
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      vehicleType: new FormControl('Auto'),
+      vehicleSubType: new FormControl(),
+    });
+
+    this.updateImagePath(this.form.value.vehicleType);
+    this.currentSubtypes = this.vehicleSubtypes['Auto'];
+
+    // Listen for changes on the vehicleType control
+    this.form.get('vehicleType')!.valueChanges.subscribe((value) => {
+      this.updateImagePath(value);
+      this.currentSubtypes = this.vehicleSubtypes[value] || [];
+    });
+  }
+
+  updateImagePath(vehicleType: string) {
+    const autoPath = '/assets/auto.jpg';
+    switch (vehicleType) {
+      case 'Auto':
+        this.imagePath = autoPath;
+        break;
+      case 'Motor':
+        this.imagePath = '/assets/motor.jpg';
+        break;
+      case 'Scooter':
+        this.imagePath = '/assets/scooter.jpg';
+        break;
+      default:
+        this.imagePath = autoPath;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    StoreModule.forRoot({}, {})
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule.forRoot({}, {}), ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
